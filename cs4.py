ec3f1cc91ec125931dbfd1d59dc941649536c692
Olov Nykvist <olov.nykvist@lybe.se>
2018-01-29 14:08:49 +0100

    Major update

--- cs4.py
+++ cs4.py
@@ -2,11 +2,14 @@
 """
     Usage:
         cs4.py gui
+        cs4.py extra
+        cs4.py iter
         cs4.py calc [--data=DATA_FILE]
 """
 from docopt import docopt
 from src.scripts.commands.calc_test import CalcTest
 from src.scripts.gui.main_kernel import MainKernel
+from src.scripts.commands.extrapolate_test import ExtrapolateTest, IterTest
 
 if __name__ == "__main__":
     args = docopt(__doc__)
@@ -15,5 +18,9 @@ if __name__ == "__main__":
         COMMAND = CalcTest(args)
     if args["gui"]:
         COMMAND = MainKernel(args)
+    if args["extra"]:
+        COMMAND = ExtrapolateTest(args)
+    if args["iter"]:
+        COMMAND = IterTest(args)
     if COMMAND:
         COMMAND.run()
